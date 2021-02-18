39e798ec8def2f582b92e36b9e2d2defa0931764
Darío Carrasquel <addario7777@gmail.com>
2021-02-17 19:30:07 -0500

    Sorting with BubbleSort - O(n^2) Time Complexity Average Scenario

---
+++ src/com/theory/concepts/BubbleSortArray.java
@@ -0,0 +1,24 @@
+package com.theory.concepts;
+
+import java.util.Arrays;
+
+public class BubbleSortArray {
+
+  public static void main(String[] args) {
+    int[] arr = new int[]{5, 4, 3};
+    System.out.println(Arrays.toString(sort(arr)));
+  }
+
+  public static int[] sort(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 1; j < arr.length -  i; j++) {
+        if (arr[j] < arr[j - 1]) {
+          int temp = arr[j];
+          arr[j] = arr[j - 1];
+          arr[j - 1] = temp;
+        }
+      }
+    }
+    return arr;
+  }
+}

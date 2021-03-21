e53578ac85e82e395212af87fdb1e758977f52d7
Darío Carrasquel <addario7777@gmail.com>
2021-03-21 12:27:38 -0500

    Longest Word Update

--- src/com/coderbyte/challenge/LongestWord.java
+++ src/com/coderbyte/challenge/LongestWord.java
@@ -1,2 +1,27 @@
-package com.coderbyte.challenge;public class LongestWord {
+package com.coderbyte.challenge;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class LongestWord {
+
+    public static String LongestWord(String sen) {
+        Pattern p = Pattern.compile("[a-zA-Z]+");
+        Matcher m = p.matcher(sen);
+        String longest = "";
+
+        while (m.find()) {
+            String match = m.group();
+            if (match.length() > longest.length()) {
+                longest = match;
+            }
+        }
+        return longest;
+    }
+
+    public static void main (String[] args) {
+        String word = "a confusing /:sentence:/[ this is not!!!!!!!~";
+        System.out.print(LongestWord(word));
+    }
 }
+

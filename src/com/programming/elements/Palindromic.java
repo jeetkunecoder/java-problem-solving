5a63abfcdbbcc73097b6156700beb824b1e2e4ce
Darío Carrasquel <addario7777@gmail.com>
2021-03-30 22:36:37 -0500

    Include time and space complexities

--- src/com/programming/elements/Palindromic.java
+++ src/com/programming/elements/Palindromic.java
@@ -10,6 +10,10 @@ public class Palindromic {
         return (new StringBuilder(str).reverse().toString().equals(str));
     }
 
+    /**
+     * Time Complexity: O(n) where n is the length of the string
+     * Space Complexity: O(1)
+     */
     public boolean isPalindromic2(String s) {
         for (int i = 0, j = s.length() - 1; i < j; i++, --j) {
             if (s.charAt(i) != s.charAt(j)) return false;

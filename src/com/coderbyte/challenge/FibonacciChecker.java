692751751ff7aed876354e9d0287ece9cbd09b45
Dario Carrasquel <addario7777@gmail.com>
2021-05-10 08:21:14 -0500

    Fibonacci Checker

--- src/com/coderbyte/challenge/FibonacciChecker.java
+++ src/com/coderbyte/challenge/FibonacciChecker.java
@@ -1,2 +1,21 @@
-package com.coderbyte.challenge;public class FibonacciChecker {
+package com.coderbyte.challenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FibonacciChecker {
+    public static String FibonacciChecker(int num) {
+        boolean res =  perfSq(4 + 5 * num * num) || perfSq(-4 + 5 * num * num);
+        return res ? "yes" : "no";
+    }
+
+    public static boolean perfSq(int num) {
+        int sq = (int) Math.sqrt(num);
+        boolean result = (sq * sq == num);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(FibonacciChecker(54));
+    }
 }

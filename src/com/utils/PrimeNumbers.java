d20ffda00bbd1f159eaa64e2fe0f1e0542cba7d7
Dario Carrasquel <addario7777@gmail.com>
2021-05-20 10:29:31 -0500

    Basic Prime Numbers

--- src/com/utils/PrimeNumbers.java
+++ src/com/utils/PrimeNumbers.java
@@ -1,2 +1,19 @@
-package com.utils;public class PrimeNumbers {
+package com.utils;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        int num = 7;
+        System.out.println(isPrime(num));
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 3) return true;
+
+        for (int i = 2; i <= (n / 2); i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
 }
